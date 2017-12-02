f365b91d49f3bb4e287e6ef31978b00b5dbe0be7
Todd Kennedy <todd@selfassembled.org>
2017-12-02 14:09:12 -0800

    day 2

--- day2/day2.rs
+++ day2/day2.rs
@@ -5,18 +5,29 @@ fn main() {
   let mut f = File::open("input.txt").expect("file not found");
   let mut contents = String::new();
   f.read_to_string(&mut contents).expect("something when wrong");
-  let rows: Vec<&str> = contents.split("\n").collect();
-  let len = rows.len();
-  let strings: Vec<Vec<&str>> = rows.iter().filter_map(|x| x.split("").collect()).collect();
+  let rows: Vec<&str> = contents.trim().split("\n").collect();
+  let mut sol1: i32 = 0;
+  let mut sol2: i32 = 0;
 
-  let mut grid: Vec<Vec<i32>> = rows.iter().split("").collect();
+  for x in 0..rows.len() {
+    let row: Vec<i32> = rows[x].split("\t").filter_map(|x| x.parse().ok()).collect();
+    let min = row.iter().min().unwrap();
+    let max = row.iter().max().unwrap();
+    sol1 = sol1 + (max - min);
 
-  for x in 0..len {
-    let row_string: Vec<_> = rows[x].split("").collect();
-    let new_row: Vec<i32> = row_string.iter().filter_map(|x| x.parse().ok()).collect();
-    grid.push(new_row)
+    let lim = row.len();
+    for i in 0..lim {
+      let v = row[i];
+      for j in 0..lim {
+        if i != j {
+          if v % row[j] == 0 {
+            sol2 = sol2 + (v / row[j]);
+          }
+        }
+      }
+    }
   }
 
-
-  println!("{:?}", grid)
+  println!("Checksum 1: {}", sol1);
+  println!("Checksum 2: {}", sol2);
 }
