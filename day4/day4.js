8870bbeb632c2a172024d169369cc6e0d66a9339
todd@selfassembled.org <todd@selfassembled.org>
2017-12-04 19:31:01 -0800

    blah blah blah

--- day4/day4.js
+++ day4/day4.js
@@ -1,15 +1,9 @@
 const fs = require('fs')
 const content = fs.readFileSync('input.txt', 'utf8').trim().split('\n')
-/*
-const content = 'aa bb cc dd ee\naa bb cc dd aa\naa bb cc dd aaa\n'.trim()
-const content = 'abcde fghij\nabcde xyz ecdab\na ab abc abd abf abj\niiii oiii ooii oooi oooo\noiii ioii iioi iiio'.trim().split('\n')
-*/
-console.log('part 1:', content.filter(line => {
-  const l = line.split(' ').map(x => x.trim())
-  return l.length === [...new Set(l)].length
-}).length)
 
-console.log('part 2:', content.filter(line => {
-  const l = line.split(' ').map(x => x.trim()).map(x => x.split('').sort().join(''))
-  return l.length === [...new Set(l)].length
-}).length)
+const compareUnique = (l) => (l.length === (new Set(l).size))
+const sortWord = (w) => w.split('').sort().join('')
+const splitLine = (l) => l.split(' ')
+
+console.log('part 1:', content.map(splitLine).filter(compareUnique).length)
+console.log('part 2:', content.map(splitLine).map(x => x.map(sortWord)).filter(compareUnique).length)
