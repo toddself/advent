9472219c075724de6e53b1d6f31060386305dd2b
Todd Kennedy <todd@selfassembled.org>
2018-12-02 15:05:42 -0800

    day 2

---
+++ day2/day2.js
@@ -0,0 +1,298 @@
+const data = `qcsnyvpigkxmrdawlfdefotxbh
+qcsnyvligkymrdawljujfotxbh
+qmsnyvpigkzmrnawzjuefotxbh
+qosnyvpigkzmrnawljuefouxbh
+qcsnhlpigkzmrtawljuefotxbh
+qcsnyvpigkzmrdapljuyfotxih
+qcsnbvpiokzmrdawljuerotxbh
+qcfnyvmigkzmrdawljuefotdbh
+qcsnynpigkzmrdawljuefptxbp
+qcsgyapigkzmrdawljuafotxbh
+qcsnyvpigkzmrdapljueeotibh
+qcfnyvpigkzmndawljuwfotxbh
+qzsayvpigkzmrdawijuefotxbh
+qcsnsvpiekzmrdawljfefotxbh
+ncsnyvpigkzmrdaaljuefotxzh
+qssnyvpigkzmrdawljuefotobg
+qcshyipigkzmrdajljuefotxbh
+qcsnyvtigkzmrdawljgeaotxbh
+qcsnkvpxgkzmrdawljuefltxbh
+qcsnyvpiikzmrdawljuwfoqxbh
+qcsnybpigwzmqdawljuefotxbh
+qcsiyvpipkzbrdawljuefotxbh
+qldnyvpigkzmrdzwljuefotxbh
+qcsnyvpwgkzcrdawljuefmtxbh
+qcsnyvnigkzmrdahmjuefotxbh
+qcsnydpigkzmrdazljuefotxnh
+qcsqyvavgkzmrdawljuefotxbh
+ucsnyvpigkzmrdawljuefocxwh
+qcsnivpigrzmrdawljuefouxbh
+tcsnyvpibkzmrdawlkuefotxbh
+qcstytpigkzmrdawsjuefotxbh
+qcynyvpigkzmrdawlluefotjbh
+qcstyvpigkqrrdawljuefotxbh
+icsnyvpizkzmrcawljuefotxbh
+qcsnyvpimkzmrdavljuezotxbh
+qvsnoupigkzmrdawljuefotxbh
+qcsnyvpigkzmrdawwjuefftxgh
+qcpnyvpijkzmrdvwljuefotxbh
+qcsnyvpigkzmxdakdjuefotxbh
+jcsvyvpigkqmrdawljuefotxbh
+qcwnyvpigczmrsawljuefotxbh
+qcsnyvpdgkzmrdawljuefoixbm
+qysnyvpigkzmrdmwljuefotxbp
+qcsnavpigkzmrdaxajuefotxbh
+qcsfkvpigkzmrdawlcuefotxbh
+qcsnyvpigkvmrdawljcefotpbh
+qcsnyvpiqkkmrdawlvuefotxbh
+qhsnyvpigkzmrdawnjuedotxbh
+qasnlvpigkzmrdawljuefotxkh
+qgsnyvpigkzmrdabpjuefotxbh
+jcsnyvdigkzmrmawljuefotxbh
+qcsnivpigkzmrdawljuefonxth
+qcsnyjpigkzmrdawljgefotxmh
+qcstyvpigkzmrdacljuefovxbh
+qcsnvvpigkzmrdawljuewotrbh
+qcsnyvaigdzmrdawljueuotxbh
+qcsnyvpegkzmwdawljzefotxbh
+qcsnevpngkzmrdawlouefotxbh
+qcsnuvpigozmrdawljuefotdbh
+qgsnyvpigkzmqdayljuefotxbh
+qcsnyvpigkzmrdcwdjuofotxbh
+qcnnyvpigkzmrzawljuefstxbh
+qlsgyvpigkzmrdtwljuefotxbh
+qcsnyfpigkzlroawljuefotxbh
+qcsnkvwigkzmrdowljuefotxbh
+qcsnrvpigkzmrdawljuvfltxbh
+qcsnyvpigkzvreawljuefotxmh
+qcsrgvpigkzmrdawliuefotxbh
+qysnyvpigkzmrdawlxaefotxbh
+qcsnyvpigizmrdlwljuefotxbi
+qzsnyvpitkzmrdawljuefbtxbh
+qzgnyvpigkzmrdawljuefotxih
+qcsnyvpigkzmrdawlguefvtxbb
+qcsnyvpigkzmidawljuefouxjh
+qksnyvpigkzmrdawlruefotxhh
+qcsnyvpinkzmrdaaljuefotxah
+qcsnxvpigkzjrdawljuefhtxbh
+qcsnyvpigkzardawlgueuotxbh
+qcsnyvpiakzmrdpwljuefotxbt
+qcsnyvpigkzmrdawkjuefotxgb
+qcsnyvpigkzmrdawljuehocsbh
+qcsnsvpigktmrdawljuefotxvh
+qusnrvpigkzrrdawljuefotxbh
+qcsnyhiigkzmrdawrjuefotxbh
+qcsnavpigkzmrdawlfuefotxbz
+qcsnyvpigkzmmdamsjuefotxbh
+qcsnyvzigkzmrdcwljmefotxbh
+qcsnyvpigkzmriawljuefotbbe
+qcsnyvpigksmrdawljaefotxbd
+qcsnyvpigkzfrdawljuefoxxmh
+qcsnyvpygkrmrdawljuefotxbi
+qcsngvwigfzmrdawljuefotxbh
+qcsnyvpigkmkrdauljuefotxbh
+qcsnyvpigxzmrdgwljuefwtxbh
+qconyapigkzmrdaxljuefotxbh
+qcsnydpigkzwrdawljulfotxbh
+qcsnyvpimkzmmdawljuefotxch
+qcsnkspigkzmrdawgjuefotxbh
+qcsnyvpigkzmrdhwljfefbtxbh
+qcsnyipijkztrdawljuefotxbh
+qcseyvpigkrhrdawljuefotxbh
+qcsnyvpivkzmrdawljuefottbb
+qcsnyvpigkzmrdawlouefcjxbh
+qcsnyvpigkzmrgayljuefotxbm
+qcsnyvpvgkzmrdawrjujfotxbh
+qcsnyvpigkzmndawljuefqtxch
+qcsnyvpigbzmrdawljuefotibg
+qcsnyvpigkzmseawljuefotxbv
+qcsnwvpigkzmraawnjuefotxbh
+mcsnyvpiqkzmrdawljuefotlbh
+bcsnyvpigczmrdmwljuefotxbh
+qcsnyvpigkzmrtawljuegntxbh
+qcsnyvpijkzmrdawlmrefotxbh
+qdsnyvpfgkzmrdawljuekotxbh
+qcsnyvpigkzmrdawcjfegotxbh
+qcslyvphgkrmddawljuefotxbh
+qcsnyvpigkzmsdawkjuefojxbh
+qzsnyvpigkzmrzawljuefmtxbh
+qcsnyvpqgkzmcdawljuefttxbh
+qcsnyvpbgkpmrdawljuefoqxbh
+qcsnyvpigkemrdywljmefotxbh
+qcsnyypigkzmrdawljmefotxwh
+jcsnyvhwgkzmrdawljuefotxbh
+qcsnyvpigkzmrdawljurlotxwh
+qcsnnvpigzzmrdawljuefotwbh
+hcsnyvpigkzmrdarljuefitxbh
+qcsnyvpilkzmrfawljuefotsbh
+qcsnynpigkzmldawijuefotxbh
+qcsnyvpkgkjmrdawljuefotxlh
+qcsnylpigkzprdawljgefotxbh
+qcsnyvpigkzmrrawljnefohxbh
+qcsnivpigkzmrqawlbuefotxbh
+qcsgyvpigkzmrfawljuefotbbh
+qccuyvpigkzmrdawyjuefotxbh
+gcsnyvpigkzjrdawljuefotxby
+qcsmyvpiekzbrdawljuefotxbh
+qcsnyvpzgkrmrdawljuefotxbs
+qesnyvpigkzmpdqwljuefotxbh
+qcsnyvpigqzmrdawljuefutibh
+qcdnyvpigkzirdawljfefotxbh
+qcsnyvpiukzmrcrwljuefotxbh
+qcsnbvpickzmrdswljuefotxbh
+qcsnyvpighzmrpadljuefotxbh
+qccnyvpigkzmrdawljudxotxbh
+qcsnyvpigkzmrdabljuesotxlh
+qcsnyvpigkzmrrawlruefozxbh
+qconyzpigkzmrdawljuefotjbh
+qclnyvpigkzmrdxwljuefotbbh
+qcsnygpigkzmrdawlhuefooxbh
+qcsnyvpigkzmvdawljuefntxnh
+qcskyvpigkzmreawljuefotubh
+qrsnyvpxgkzmrdawljuefotxbz
+qclnyvpigtamrdawljuefotxbh
+qcsnyvpigkzmrdawojxefoyxbh
+qcsnyvpinkzmrdakljuwfotxbh
+qcsnyvpiykzmedawljuefgtxbh
+qcsayvpigkcmrdawijuefotxbh
+qcsnyvuiekzmrdamljuefotxbh
+qcdnyvpigkzmrdawnjuefoxxbh
+qcsnfvpwgszmrdawljuefotxbh
+qcsnycpigkzmrdawljqefotxih
+qcslyvphgkrmrdawljuefotxbh
+ecsnyvpigkzmrdawykuefotxbh
+qcsayvpigkzmraawljuekotxbh
+qcsnyvpigkdmrdawljuewofxbh
+qcznyvpigkzqrdawljuefotxnh
+qcsnyvplgkzmrdawljiefotlbh
+qcsnyvpigkzmroewljuefotbbh
+qcvnyvpigkzvrdawujuefotxbh
+qcanyypigkzmrdaeljuefotxbh
+qcsnyvwigkzmrdewljuefotxqh
+qcsryvpigkvmrdawljuefotabh
+pcsnyvpigkwmrdawljueforxbh
+qcsncvpigkzmrdawljuefotwmh
+qcsnyvpigozmrdawljudfozxbh
+qcsnynpigkzmrbawhjuefotxbh
+qcsnyvuigkzmrqawljuefotxch
+qcsnyvpickzmrdawljueeofxbh
+qcsnyvpigkzgrdawljueiouxbh
+qcsnyvpigkztrdawljuxnotxbh
+qcsnyvpigwzvrdawljfefotxbh
+qcsnyvpilkzmrdawljuefotxcz
+qcsnjvpigkzmrdawljuefoywbh
+qhsnyvpigyzmrdawljuhfotxbh
+qcsnyvpirkzmfdawljuffotxbh
+qcsjyvpigkzmvdawljuefotxzh
+qcszivpirkzmrdawljuefotxbh
+qwsnyvpigkzmtdawljuefetxbh
+qcrntvpigkzordawljuefotxbh
+qrsnyvpigkzmsdawljrefotxbh
+qcsnyviivkzmrdazljuefotxbh
+ecsnyvpigkzmrdawyjuefotxbw
+qnsnyvpkgkzmrdawljueqotxbh
+qcsyyppigkzmrdawljuefotxba
+qcsnyvpigkzhrdpwljuefouxbh
+ucsnyvpigkzmrdawojuefouxbh
+qysnyvpigkzmrdawljukfotxbd
+qcjnyvpigkzmrdalljfefotxbh
+fcsnyapigkmmrdawljuefotxbh
+qcnnkvpigkzmrdawljuefctxbh
+ocsnyvpigkzmsdawljuefotxbl
+qcsnyvpiakomrdawpjuefotxbh
+qcsnyvpigkzmrdawljvefbtxwh
+qcsnuvpigkzmvdfwljuefotxbh
+qcsnyapihkzmrdagljuefotxbh
+qzsnyvpigkzmrdawtjuefotxgh
+qcsnyvpigkzmrdawljuefomyah
+ocsnyvpigkzqrdawljuefotxbt
+qnsnyvpigkzmrdawljvevotxbh
+icsnyvpigkzmrdawljuefntxbt
+qcsnyvpigkzdrdawljuefotbbm
+scsnyvpigkzmrgawljuofotxbh
+qcsnydpigkzmrdowljuefotkbh
+qcsnyvtikkzmrdawljuefolxbh
+qcsiyvpigkcmrddwljuefotxbh
+qyrnyvpigkzmodawljuefotxbh
+pcsndvpfgkzmrdawljuefotxbh
+qcsnyvkigkhmriawljuefotxbh
+qcsnyvpigkzmsdmwlkuefotxbh
+dosnyvpigkzmrdawdjuefotxbh
+qcnnnvpigkzmrdzwljuefotxbh
+qcsnyvpivkumrdailjuefotxbh
+qcsnyvpigkzmrdswljuzfotxbz
+qcscynpigkzmrdawljuefotxbc
+qeanyvpigkzmrdawijuefotxbh
+qclnylpigkzmrdawljuefotxyh
+qcsnyvpigkzmrdawljbefowxbp
+qcsnyvpagkzmrdawljuefolebh
+qxsiyvpigkzmrdawljuefotxgh
+qcsnyvpigkynrdawljuefoqxbh
+qcsnevpigkzmrdxwgjuefotxbh
+qcsnyvpdgkzlrdawljeefotxbh
+qcsnyvpigkzmrgawljxbfotxbh
+ecsnyvpigkzmrdbwbjuefotxbh
+qcsnyvpigkzmraawujuefocxbh
+qcsnyvpihkzmrdawljuefouxbn
+fgsqyvpigkzmrdawljuefotxbh
+qcsnyvpigkmmrdawajuefotnbh
+qcsnyvvigkzmrdahljudfotxbh
+qcsnyvpixkzmrdqwljutfotxbh
+ncsnyvpickzmrdawljuehotxbh
+qcsnyvpizkzmrdawlpuefotxbp
+wcsnyvfigkzmrdakljuefotxbh
+qcsnyvpigkznrdhwljupfotxbh
+jcsnyvpigkpmzdawljuefotxbh
+qcsnyppigkkmrdawljujfotxbh
+qcsnyvpigkumrdaeljuefodxbh
+qcsnyvhigkzmrdrwljuefodxbh
+qcsnyvpigkacrdawtjuefotxbh
+qcsnyvpigkzmylawlquefotxbh`
+
+const twice = []
+const thrice = []
+
+for (const box of data.trim().split('\n')) {
+  let match2 = false
+  let match3 = false
+  for (const char of box.split('')) {
+    const re = new RegExp(char, 'g')
+    if (!match3 && box.match(re).length === 3) {
+      thrice.push(box)
+      match3 = true
+    } else if (!match2 && box.match(re).length === 2) {
+      twice.push(box)
+      match2 = true
+    }
+  }
+}
+
+console.log('part 1 twice', twice.length, 'thrice', thrice.length, 'checksum', twice.length * thrice.length)
+
+const source = data.trim().split('\n').filter((box) => twice.includes(box) || thrice.includes(box))
+const boxes = []
+
+for (let j = 0, len = source.length; j < len; j++) {
+  const box = source[j]
+  for (let k = 0, len = source.length; k < len; k++) {
+    if (j === k) continue
+    const box2 = source[k]
+    let diffCount = 0
+    for (let i = 0, len = box.length; i < len; i++) {
+      if (box[i] !== box2[i]) {
+        ++diffCount
+      }
+    }
+    if (diffCount === 1) {
+      boxes.push(box)
+      boxes.push(box2)
+    }
+  }
+}
+
+const matches = [...new Set(boxes)]
+const word = []
+for (let i = 0, len = matches[0].length; i < len; i++) {
+  if (matches[0][i] === matches[1][i]) word.push(matches[0][i])
+}
+console.log('part 2', word.join(''))
