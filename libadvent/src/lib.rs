442cdb66c0c40779f57789f027b837e6d07220bf
Todd Kennedy <todd@selfassembled.org>
2021-12-02 13:36:03 -0800

    optional filepath

--- libadvent/src/lib.rs
+++ libadvent/src/lib.rs
@@ -1,20 +1,24 @@
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 use std::str::FromStr;
 
-pub fn get_data() -> Result<String, std::io::Error> {
-    let filename = match env::current_dir() {
-        Ok(mut cwd) => {
-            cwd.push("input");
-            cwd
-        }
-        Err(srsly) => panic!("{:?}", srsly),
+pub fn get_data(fname: Option<PathBuf>) -> Result<String, std::io::Error> {
+    let filename = match fname {
+        Some(fname) => fname,
+        None => match env::current_dir() {
+            Ok(mut cwd) => {
+                cwd.push("input");
+                cwd
+            }
+            Err(srsly) => panic!("{:?}", srsly),
+        },
     };
     fs::read_to_string(filename)
 }
 
 pub fn get_data_as_vec<T: FromStr>() -> Vec<T> {
-    let data = get_data().unwrap();
+    let data = get_data(None).unwrap();
     data.split("\n")
         .filter_map(|e| e.parse::<T>().ok())
         .collect()
